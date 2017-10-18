84293add79829a80b239c2582194135d2eb679b2
Joey Jirasevijinda <joey.jira@gmail.com>
2017-10-18 01:09:41 -0700

    created user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,26 @@ class User < ApplicationRecord
         user = User.find_by_username(username)
         return nil unless user && user.is_password?(password)
         user
+    end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end 
+
+    private
+
+    def ensure_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+
+    def reset_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save
+        self.session_token
+    end
 end
